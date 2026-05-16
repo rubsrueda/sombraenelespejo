0aaa839079ea607e9851df9e128f552090459614
rubsrueda <rueda.castillo@gmail.com>
2026-05-16 03:43:34 +0000

    Autorizar rueda.castillo@gmail.com como admin

--- admin-config.js
+++ admin-config.js
@@ -1,5 +1,5 @@
 // Emails autorizados para abrir el panel admin de afiliados.
 // Reemplaza con tus correos reales de administrador.
 export const ADMIN_EMAILS = [
-  // "tu-email@dominio.com",
+  "rueda.castillo@gmail.com",
 ];
